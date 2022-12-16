e7dc4aad201bdff744977be33214f70c3f655246
Ömer Can Uyar <omercanuyar@ogr.iu.edu.tr>
2022-12-16 19:08:20 +0300

    printing string failed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
+    public static int simetrik(String s){
+        int top_size=0;
+        for (int i =0;i<s.length();i++){ //tüm harfler için döngü oluşturuyoruz
+
+            int size =1;
+            for(int cap=1;cap<=i;cap++){
+                if(i-cap>=0 && i+cap<s.length()){
+                    if(s.charAt(i-cap)==s.charAt(i+cap)){
+                        size=size+2;
+                    }
+                    else{
+                        if(size>top_size){
+                            top_size=size;
+                            break;
+                        }
+                    }
+                }
+
+            }
+
+        }
+        return top_size;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(simetrik("FourscoreandsevenyearsagoourfaathersbroughtforthonthiscontainentanewnationconceivedinzLibertyanddedicatedtothepropositionthatallmenarecreatedequalNowweareengagedinagreahtcivilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"));
     }
 }
