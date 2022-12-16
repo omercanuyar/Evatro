8cd7e4f758ed9bf0c0ae11d243a95de7d76c9c0d
Ömer Can Uyar <omercanuyar@ogr.iu.edu.tr>
2022-12-16 19:27:15 +0300

    last

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,28 @@
 public class Main {
-    public static int simetrik(String s){
+    public static String simetrik(String s){
         int top_size=0;
-        for (int i =0;i<s.length();i++){ //tüm harfler için döngü oluşturuyoruz
+        String biggest_str="";
 
+        // bütün harfler için bir döngü oluşturuyoruz
+        for (int i =0;i<s.length();i++){
             int size =1;
+            String sub=""+s.charAt(i);
+
+            //her harf için bir sonraki ve bir önceki harf eşleştirmeleri yapılıyor
+            //iki harf öncesi ve iki harf sonrası şeklinde eşleşmeleri yapıyor
+            //eşleşme sürene kadar devam ediyor
+            //eşleşme olmadığı esnada kesip harflerin kaydını bitiriyor
+
             for(int cap=1;cap<=i;cap++){
                 if(i-cap>=0 && i+cap<s.length()){
                     if(s.charAt(i-cap)==s.charAt(i+cap)){
                         size=size+2;
+                        sub=s.charAt(i-cap)+sub+s.charAt(i-cap);
                     }
                     else{
                         if(size>top_size){
                             top_size=size;
+                            biggest_str=sub;
                             break;
                         }
                     }
@@ -20,9 +31,11 @@ public class Main {
             }
 
         }
-        return top_size;
+        return biggest_str;
     }
     public static void main(String[] args) {
+        //yazdırıyoruz
         System.out.println(simetrik("FourscoreandsevenyearsagoourfaathersbroughtforthonthiscontainentanewnationconceivedinzLibertyanddedicatedtothepropositionthatallmenarecreatedequalNowweareengagedinagreahtcivilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"));
+
     }
 }
